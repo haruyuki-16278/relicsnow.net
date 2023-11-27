56d56d04fca05f027ff9c323fdd945961506f83a
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2023-11-28 00:11:59 +0900

    コンポーネントを追加

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'rs-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+}
