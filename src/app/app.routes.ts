ad3d3873b406da8b8d3fc773dfaf7d2524890b67
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2023-11-30 23:25:54 +0900

    submoduleを追加してディレクトリ構造を変更

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { ListComponent } from './list/list.component';
+import { ListComponent } from './components/list/list.component';
 
 export const routes: Routes = [
   {
