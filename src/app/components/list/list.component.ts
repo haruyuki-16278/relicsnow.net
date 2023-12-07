52274856f9ac31ae48fc840b263fdee242ebb1ef
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2023-12-07 22:38:30 +0900

    prettierを有効に

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Article, isArticle } from '../../types/articles.type';
 
 @Component({
   selector: 'rs-list',
   standalone: true,
   imports: [CommonModule],
+  providers: [HttpClient],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
-export class ListComponent {
-  
+export class ListComponent implements OnInit {
+  articles: Article[] = [];
+
+  constructor(private readonly httpClient: HttpClient) {}
+
+  ngOnInit() {
+    this.httpClient
+      .get('assets/articles/article-list.json')
+      .subscribe((response) => {
+        console.log(response);
+        Array(response).forEach(article => {
+          if (isArticle(article)) this.articles.push(article)
+        });
+        console.log(this.articles);
+      });
+  }
 }
