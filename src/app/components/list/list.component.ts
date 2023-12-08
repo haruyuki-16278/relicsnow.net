ae67386e3c65e148c4984bbea812eac033d2fe3b
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2023-12-09 00:31:19 +0900

    一覧を実装

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -20,11 +20,23 @@ export class ListComponent implements OnInit {
     this.httpClient
       .get('assets/articles/article-list.json')
       .subscribe((response) => {
-        console.log(response);
-        Array(response).forEach(article => {
-          if (isArticle(article)) this.articles.push(article)
-        });
+        if (Array.isArray(response)) {
+          response.forEach(article => {
+            if (isArticle(article)) {
+              article.created_at = new Date(article.created_at)
+              if (article.updated_at) article.updated_at = new Date(article.updated_at)
+              this.articles.push(article)
+            }
+          });
+        }
         console.log(this.articles);
       });
+  }
+
+  getDateLabel (article: Article): string {
+    const createdAt = `${article.created_at.getFullYear()}/${article.created_at.getMonth() + 1}/${article.created_at.getDate()}`
+    let updatedAt: string | undefined = undefined
+    if (article.updated_at) updatedAt = `${article.updated_at.getFullYear()}/${article.updated_at.getMonth()+1}/${article.updated_at.getDate()}`
+    return createdAt + `${updatedAt ? `(更新: ${updatedAt})` : ''}`
   }
 }
