ef947b345732bd3142c3be59e66379605c3588c0
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2023-12-09 22:16:45 +0900

    ルーティングを更新

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,11 @@ import { ListComponent } from './components/list/list.component';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'blogs/:page',
     component: ListComponent
+  }, {
+    path: '',
+    redirectTo: '/blogs/1',
+    pathMatch: 'full'
   }
 ];
