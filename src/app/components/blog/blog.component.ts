6916f4ad37a4f3e3a2794e0f3d9b5e7fa8664a09
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2024-01-02 02:03:15 +0900

    highlight.jsを導入

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { marked } from 'marked';
+import hljs from 'highlight.js';
 
 @Component({
   selector: 'rs-blog',
@@ -29,6 +30,13 @@ export class BlogComponent implements OnInit {
             this.article = `<main>${await marked(
               response.replace(/---[\s\S\n]*---\n/g, ''),
             )}</main>`.replace('src="imgs/', 'src="assets/articles/imgs/');
+            const codes = [...this.article.matchAll(/<code(.*)>([\s\S\n]*?)<\/code>/g)];
+            console.log(codes);
+            codes.forEach((code) => {
+              const format = [...code[1].matchAll(/class="language-(.+)"/g)]?.[0]?.[1];
+              const highlighted = hljs.highlight(code[2], { language: format ?? 'javascript' });
+              this.article = this.article.replace(code[0], `<code>${highlighted.value}</code>`);
+            });
             console.log(this.article);
           });
       }
