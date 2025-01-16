f3a4662da309203671dce4f84c9d7d3ca34d7976
haruyuki@macmini <haruyuki@relicsnow.info>
2025-01-17 07:57:33 +0900

    レイアウトコンポーネントを追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
-  template: `
-    <header class="flex items-center justify-between">
-      <h1 class="bg-marker-main text-3xl font-bold md:text-4xl">
-        <a routerLink="/">relicsnow.net</a>
-      </h1>
-
-      <nav class="flex gap-2">
-        <a class="link-marker" href="https://github.com/haruyuki-16278" target="_blank">github</a>
-        <a class="link-marker" href="https://twitter.com/haruyuki_16278" target="_blank"> Χ </a>
-      </nav>
-    </header>
-
-    <main class="container-base">
-      <router-outlet />
-    </main>
-
-    <footer class="flex items-center justify-center gap-2">
-      <small class="w-72 shrink-0 text-text-sub">
-        当サイトの内容は、特別に表記がない限り<br /><a
-          href="https://creativecommons.org/licenses/by/4.0/deed.ja"
-          >クリエイティブ・コモンズ 表示 4.0 ライセンス</a
-        ><br />の下に提供されています。
-      </small>
-      <a class="non-underline" href="https://creativecommons.org/licenses/by/4.0/deed.ja">
-        <img
-          class="w-20"
-          src="https://mirrors.creativecommons.org/presskit/buttons/88x31/svg/by.svg"
-        />
-      </a>
-    </footer>
-  `,
+  imports: [LayoutComponent],
+  template: `<app-layout />`,
 })
 export class AppComponent {}
