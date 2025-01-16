12d358c0ef3d6ffbbf5f5be8ebdef0157bfc50c1
haruyuki@macmini <haruyuki@relicsnow.info>
2025-01-17 07:50:05 +0900

    デザインを過去のブログに揃えた

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,24 +6,48 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterLink, RouterOutlet],
   template: `
-    <nav>
-      <a routerLink="/">Home</a>
-    </nav>
+    <header class="flex justify-between items-center">
+      <h1 class="text-3xl font-bold bg-marker-main md:text-4xl">
+        <a routerLink="/">relicsnow.net</a>
+      </h1>
 
-    <router-outlet />
-  `,
-  styles: `
-    :host {
-      max-width: 1280px;
-      margin: 0 auto;
-      padding: 2rem;
-      text-align: center;
-    }
+      <nav class="flex gap-2">
+        <a
+          class="link-marker"
+          href="https://github.com/haruyuki-16278"
+          target="_blank"
+        >github</a>
+        <a
+          class="link-marker"
+          href="https://twitter.com/haruyuki_16278"
+          target="_blank"
+        >
+          Χ
+        </a>
+      </nav>
+    </header>
 
-    nav {
-      text-align: left;
-      padding: 0 0 2rem 0;
-    }
+    <main class="container-base">
+      <router-outlet />
+    </main>
+
+    <footer class="flex gap-2 justify-center items-center">
+      <small class="shrink-0 w-72 text-text-sub">
+        当サイトの内容は、特別に表記がない限り<br /><a
+          href="https://creativecommons.org/licenses/by/4.0/deed.ja"
+          >クリエイティブ・コモンズ 表示 4.0 ライセンス</a
+        ><br />の下に提供されています。
+      </small>
+      <a
+        class="non-underline"
+        href="https://creativecommons.org/licenses/by/4.0/deed.ja"
+      >
+        <img
+          class="w-20"
+          src="https://mirrors.creativecommons.org/presskit/buttons/88x31/svg/by.svg"
+        />
+      </a>
+    </footer>
   `,
 })
 export class AppComponent {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./index.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main': 'var(--color-main)',
+        'sub': 'var(--color-sub)',
+        'text': 'var(--color-text)',
+        'text-sub': 'var(--color-text-sub)',
+        'link': 'var(--color-link)',
+        'focused': 'var(--color-focused)',
+        'background': 'var(--color-background)',
+      },
+      backgroundImage: {
+        'marker-main': 'linear-gradient(transparent 50%, var(--color-main) 50%, var(--color-main) 80%, transparent 80%)',
+        'marker-sub': 'linear-gradient(transparent 50%, var(--color-sub) 50%, var(--color-sub) 80%, transparent 80%)',
+        'marker-link': 'linear-gradient(transparent 50%, var(--color-link) 50%, var(--color-link) 80%, transparent 80%)',
+        'marker-focused': 'linear-gradient(transparent 50%, var(--color-focused) 50%, var(--color-focused) 80%, transparent 80%)',
+        'marker': 'linear-gradient(transparent 50%, var(--marker-color) 50%, var(--marker-color) 80%, transparent 80%)',
+      }
+    },
   },
   plugins: [],
 }
