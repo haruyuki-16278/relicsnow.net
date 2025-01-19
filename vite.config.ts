938ca87b59a84567337cb3540d81a14646e3ab23
haruyuki@macmini <haruyuki@relicsnow.info>
2025-01-19 21:03:18 +0900

    画像の読み込みがうまく行ってなかったのを修正

--- vite.config.ts
+++ vite.config.ts
@@ -26,6 +26,8 @@ export default defineConfig(async ({ mode }) => {
     resolve: {
       mainFields: ['module'],
     },
+    publicDir: 'src/assets',
+    assetsInclude: ['**/*.jpg', '**/*.png', '**/*.gif', '**/*.webp', '**/*.svg', '**/*.ico'],
     plugins: [
       analog({
         content: {
